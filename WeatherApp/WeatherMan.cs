cac2c6541a28fcc62453dcbe25552784f230fdd5
E-v7 <33555329+E-v7@users.noreply.github.com>
2023-12-09 21:27:56 -0500

    Git ignore file updated with important files to ignore

---
+++ WeatherApp/WeatherMan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeatherApp.Properties;
+
+namespace WeatherApp {
+    public class WeatherMan {
+        static WeatherMan() {
+            Settings settings = new Settings();
+            var tmp = settings.TestSetting;
+        }
+    }
+}
