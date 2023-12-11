0e2caa59d36047bc3dc12604a9ce204f149a595a
Kushika Senera <kushikasenera@gmail.com>
2023-12-10 23:57:47 -0500

    new test suite for parsing json and generating html for weather cards on current index page where user enters city to find weather data

--- WeatherAppUnitTests/indexTests.cs
+++ WeatherAppUnitTests/indexTests.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Net;
+using WeatherApp;
+using static WeatherApp.index;
 
 namespace WeatherAppUnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class indexTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void JsonParse_ValidData_ReturnsCorrectForecasts()
         {
+            // Arrange
+            var processor = new ForecastDataProcessor();
+            string validJson = "{ \"list\": [{ \"dt\": 1618308000, \"main\": { \"temp\": 15 }, \"weather\": [{ \"description\": \"clear sky\" }], \"wind\": { \"speed\": 1.5 }, \"rain\": { \"3h\": 0.0 } }] }";
+
+            // Act
+            var result = processor.JsonParse(validJson);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(1618308000), result[0].Time);
+            Assert.AreEqual(15, result[0].Temperature);
+            Assert.AreEqual("clear sky", result[0].Description);
+            Assert.AreEqual(1.5, result[0].WindSpeed);
+            Assert.AreEqual(0.0, result[0].Precipitation);
+
         }
     }
 }
+
