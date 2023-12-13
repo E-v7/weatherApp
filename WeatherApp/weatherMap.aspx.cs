ca11265898036f6fd2352fa0f3d48c81f36087ab
ashwinigunaga <gunagaashwini@gmail.com>
2023-12-12 21:17:46 -0500

    Get the latitude and longitude of the location on page load

---
+++ WeatherApp/weatherMap.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WeatherApp
+{
+    public partial class weatherMap : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Weather weatherDetails = WeatherWizard.GetCurrentWeatherToWeatherObject("Bangalore");
+            double latitude = weatherDetails.coord.lat;
+            double longitude = weatherDetails.coord.lon
+
+        }
+    }
+}
