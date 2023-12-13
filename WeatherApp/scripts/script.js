5bbedfcc8cbcb13506f6699874d1909a34a7aad4
ksenera <ksenera7130@conestogac.on.ca>
2023-12-12 19:01:15 -0500

    added the ready function to implement the geolocation consent ui and AJAX call to retrieve user's location weather details if they consent to share their location

--- WeatherApp/scripts/script.js
+++ WeatherApp/scripts/script.js
@@ -1,1 +1,11 @@
-﻿
+﻿// jQuery ready function to attach event listeners
+$(document).ready(function () {
+    $('#consentToggle').change(function () {
+        if (this.checked) {
+            getLocationAndWeather();
+        } else {
+            // Hide the weather details if user toggles off consent
+            $('#weatherDetails').hide();
+        }
+    });
+});
