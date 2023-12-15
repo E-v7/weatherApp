6a8d4a599cd552696ffa1c6c9f47e1466f68c51c
Thomas audet <thomas.audet@live.com>
2023-12-14 21:14:59 -0500

    added bsic connection function

--- WeatherApp/AccountServicing.cs
+++ WeatherApp/AccountServicing.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
-using System.Web;
-using System.Web.UI.WebControls;
+using Dapper;
+using MySql.Data.MySqlClient;
 
 namespace WeatherApp
 {
@@ -12,22 +16,86 @@ namespace WeatherApp
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
-        public string Email { get; set; }
         public string PasswordHash { get; set; }
+        public string Email { get; set; }
+        // Additional properties can be added here as needed
     }
-
     public class AccountServicing
     {
         private readonly List<User> users = new List<User>(); // Simulated database
 
+        private readonly string connectionString;
         // Strong password regex pattern
         private readonly Regex passwordRegex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{12,}$");
 
-        public bool ValidateLogin(string username, string password)
-        {
 
-            return PlaceHolder;
+        public bool CreateAccount(string firstName, string lastName, string username, string password, string email)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(email))
+            {
+                throw new ArgumentException("Username, password, and email cannot be blank.");
+            }
+            if (!passwordRegex.IsMatch(password))
+            {
+                throw new ArgumentException("Password does not meet security requirements.");
+            }
+            if (users.Any(u => u.Username == username || u.Email == email))
+            {
+                throw new ArgumentException("An account with this username or email already exists.");
+            }
+            var newUser = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Username = username,
+                Email = email,
+                PasswordHash = HashPassword(password)
+            };
+            users.Add(newUser);
+            return true;
+        }
+        public bool ValidateLogin(string usernameOrEmail, string password)
+        {
+            var user = users.FirstOrDefault(u => u.Username == usernameOrEmail || u.Email == usernameOrEmail);
+            if (user == null)
+            {
+                return false; // User not found
+            }
+            // Check if the hashed password matches
+            return VerifyPassword(password, user.PasswordHash);
+        }
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        private bool VerifyPassword(string enteredPassword, string storedHash)
+        {
+            string hashOfEnteredPassword = HashPassword(enteredPassword);
+            return hashOfEnteredPassword == storedHash;
         }
 
+        private bool verifyuserName(string userName)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(getConnectionString("WeatherUserDB")))
+            {
+                connection.Query($"select ").ToList();
+                return true;
+            }
+        }
+
+
+        private static string getConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
     }
 }
