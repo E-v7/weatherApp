24c63a939a19440356f171dd1c11e67986d5c96f
ashwinigunaga <gunagaashwini@gmail.com>
2023-12-15 02:04:04 -0500

    Changes to zoom in to user's current location

--- WeatherApp/index.aspx.cs
+++ WeatherApp/index.aspx.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Services;
@@ -15,17 +16,64 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using WeatherApp.Properties;
 
-namespace WeatherApp {
-    public partial class index : System.Web.UI.Page 
+namespace WeatherApp
+{
+    public partial class index : System.Web.UI.Page
     {
+        //protected AccountServicing accountService;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //accountService = new AccountServicing();
+            // additional page load logic if necessary
+        }
         [WebMethod]
         public static string GetWeather(string lat, string lon)
         {
-            // Call the WeatherWizard class method to get the current weather
+            // call the WeatherWizard class method to get the current weather
             JObject weatherData = WeatherWizard.GetCurrentWeatherToJObject(lat, lon);
 
-            // Convert the JObject to a JSON string to send back to the client
+            // convert the JObject to a JSON string to send back to the client
             return weatherData.ToString();
+        }
+
+        public void Login_Click(object sender, EventArgs e)
+        {
+            string username = userName.Text.Trim();
+            string password = passWord.Text;
+
+            // Call AccountServicing methods for validation (to be implemented)
+            // This will eventually check if the user exists in the database and validate the password
+            //bool isValidUser = accountService.ValidateLogin(username, password);
+            bool isValidUser = false;
+
+            if (isValidUser)
+            {
+                // proceed with successful login logic
+                // for example: Redirect to a different page or display a success message
+            }
+            else
+            {
+                // handle failed login attempt
+                // display an error message to the user
+            }
+
+
+        }
+
+        public void Register_Click(object sender, EventArgs e)
+        {
+            string firstname = firstName.Text.Trim();
+            string lastname = lastName.Text.Trim();
+            string email = emailAddress.Text.Trim();
+            string username = userName.Text.Trim();
+            string password = passWord.Text.Trim();
+            string confirmPw = confirmPassword.Text.Trim();
+
+            if (password == confirmPw)
+            {
+
+            }
         }
     }
 }
--- WeatherApp/weatherMap.aspx.cs
+++ WeatherApp/weatherMap.aspx.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WeatherApp.Properties;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WeatherApp
 {
@@ -15,27 +18,51 @@ namespace WeatherApp
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
-            {
-                Weather weatherDetails = WeatherWizard.GetCurrentWeatherToWeatherObject("bangalore");
-                double latitude = weatherDetails.coord.lat;
-                double longitude = weatherDetails.coord.lon;
-                string description = weatherDetails.weather[0].description;
-                string script = "initMap(" + latitude + ", " + longitude + ", '" + apiKey + "', '" + description + "');";
+            { 
+                string script = "getLocationAndWeather('"+apiKey+"');";
                 ScriptManager.RegisterStartupScript(this, GetType(), "InitializeMap", script, true);
+                
             }
+        }
+
+        protected void GetWeather(string lat, string lon)
+        {
+            // call the WeatherWizard class method to get the current weather
+            
+            Weather weatherDetails = WeatherWizard.GetCurrentWeatherToWeatherObject(lat,lon);
+            double latitude = weatherDetails.coord.lat;
+            double longitude = weatherDetails.coord.lon;
+            string description = weatherDetails.weather[0].description;
+            string script = "initMap(" + lat + ", " + lon + ", '" + apiKey + "', '" + description + "');";
+            ScriptManager.RegisterStartupScript(this, GetType(), "InitializeMap", script, true);
+            string scriptAlert = "alert('Unable to get weather for this location!');";
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", scriptAlert, true);
+            // convert the JObject to a JSON string to send back to the client
+            // return weatherData.ToString();
         }
 
         protected void GoToLocation(object sender, EventArgs e)
         {
             string city = Request.Form["city"];
+            errorText.Text = "";  //clear previous error messages
             if (city != "")
             {
+                
                 Weather weatherDetails = WeatherWizard.GetCurrentWeatherToWeatherObject(city);
-                double latitude = weatherDetails.coord.lat;
-                double longitude = weatherDetails.coord.lon;
-                string description = weatherDetails.weather[0].description;
-                string script = "initMap(" + latitude + ", " + longitude + ", '" + apiKey + "', '" + description + "');";
-                ScriptManager.RegisterStartupScript(this, GetType(), "InitializeMap", script, true);
+                if (weatherDetails != null) {
+                    double latitude = weatherDetails.coord.lat;
+                    double longitude = weatherDetails.coord.lon;
+                    string description = weatherDetails.weather[0].description;
+                    string script = "initMap(" + latitude + ", " + longitude + ", '" + apiKey + "', '" + description + "');";
+                    ScriptManager.RegisterStartupScript(this, GetType(), "InitializeMap", script, true);
+                }
+                else {
+
+                    string script = "alert('Unable to get weather for this location!');";
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", script, true);
+                    string scriptMap = "getLocationAndWeather();";
+                    ScriptManager.RegisterStartupScript(this, GetType(), "InitializeMap", scriptMap, true);
+                }
             }
         }
     }
